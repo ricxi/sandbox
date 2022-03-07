f672887ce1ff60b15977d65f369ea83eb4f2025e
irixoc <irixoc@gmail.com>
2022-03-07 06:08:58 -0500

    tested out an api call to get a token from github

--- gobox/api_call/main.go
+++ gobox/api_call/main.go
@@ -1,7 +1,35 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+)
 
 func main() {
-	fmt.Println("didn't know I could nest stuff like this")
+	token, err := getToken()
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(token)
+	
+	// resp, err := http.Get("https://api.github.com/users")
+	// if err != nil {
+	// 	fmt.Println(err)
+	// }
+	// defer resp.Body.Close()
+	
+	// respBody, err := ioutil.ReadAll(resp.Body)
+	
+	// fmt.Print(string(respBody))
+}
+
+// maybe I should be returning a string and an error
+func getToken() (string, error) {
+	token, ok := os.LookupEnv("GITHUB_TOKEN")
+	if !ok {
+		fmt.Println("Token not found")
+		return "", fmt.Errorf("ERROR: unable to find token")
+	}
+	return token, nil
 }
